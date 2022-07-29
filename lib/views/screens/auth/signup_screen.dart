562935fbf64839ab63526eed162bf51432c5485a
sandeep <sksandeep9215@gmail.com>
2022-07-29 21:08:57 +0530

    working on ui of signup screen

---
+++ lib/views/screens/auth/signup_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../widgets/custom_text_input_field.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({Key? key}) : super(key: key);
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Tik-Tok Clone",
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: buttonColor,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                Text(
+                  "Register User",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: buttonColor,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                Stack(
+                  children: const [
+                    CircleAvatar(
+                      radius: 64,
+                      backgroundColor: Colors.black,
+                      backgroundImage: NetworkImage(
+                          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: Icon(Icons.camera_alt),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: CustomTextInputField(
+                    controller: _usernameController,
+                    icon: Icons.person,
+                    isObscure: false,
+                    labelText: "UserName",
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: CustomTextInputField(
+                    controller: _emailController,
+                    icon: Icons.email,
+                    isObscure: false,
+                    labelText: "Email",
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: CustomTextInputField(
+                    controller: _passwordController,
+                    icon: Icons.lock,
+                    isObscure: true,
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 30,
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  decoration: BoxDecoration(
+                    color: buttonColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: InkWell(
+                    onTap: () {},
+                    child: const Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "If you already have Account go to ",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 20, color: buttonColor),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
