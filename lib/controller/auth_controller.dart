c07fc2a0cdd7f860f4288933802a6de7119ff2ee
sandeep <sksandeep9215@gmail.com>
2022-07-30 07:33:31 +0530

    logic in auth controller for upload user profile pic and get userDownloadUrl

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/constants.dart';
+
+class AuthController extends GetxController {
+  void registerUser(
+    String username,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        //save user to auth and firebase
+        UserCredential credential =
+            await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String userDownloadUrl = await _uploadUserProfileImageToFirebase(image);
+
+
+
+        //**///
+      }
+    } catch (e) {
+      Get.snackbar("Error Creating Account", e.toString());
+    }
+  }
+
+  Future<String> _uploadUserProfileImageToFirebase(File image) async {
+    /*Reference=>Represents a reference to a Google Cloud Storage object.
+    Developers can upload, download, and delete objects, as well as
+     get/set object metadata.*/
+    Reference reference = firebaseStorage
+        .ref()
+        .child('userProfilePic')
+        .child(firebaseAuth.currentUser!.uid);
+//Upload a File from the filesystem. The file must exist.
+    UploadTask uploadTask = reference.putFile(image);
+//A TaskSnapshot is returned as the result or on-going process of a Task
+    TaskSnapshot taskSnapshot = await uploadTask;
+
+    String userDownloadUrl = await taskSnapshot.ref.getDownloadURL();
+    return userDownloadUrl;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
-import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
+import 'package:tiktok_clone/controller/auth_controller.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -12,12 +18,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'TikTok App',
       debugShowCheckedModeBanner: false,
       theme:
           ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
-      home:  SignupScreen(),
+      home: SignupScreen(),
     );
   }
 }
